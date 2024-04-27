d3b385337c1e5e157f29dbe9d437e5e0c1167b75
Haolinzzz <hz2508@nyu.edu>
2024-04-27 10:17:13 -0400

    energy conservation check

--- planets.py
+++ planets.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from astroquery.jplhorizons import Horizons
-
+from math import sqrt
 
 G           = 6.67e-11
 Ms          = 2.0e30             # sun
@@ -75,6 +75,23 @@ class Probe:
 
         return px,py,pz
     
+    # Validation for energy conservation
+    def total_mechanical_energy(self, planets, ptr):
+        # kinetic energy
+        speed = sqrt(self.xv**2 + self.yv**2 + self.zv**2)
+        kinetic_energy = 0.5 * self.mass * speed**2
+        
+        # potential energy
+        potential_energy = 0
+        for planet in planets:
+            distance = sqrt((self.x - planet.x[ptr])**2 + (self.y - planet.y[ptr])**2 + (self.z - planet.z[ptr])**2)
+            if distance != 0:
+                potential_energy += -G * self.mass * planet.mass / distance
+
+        # Total mechanical energy
+        total_energy = kinetic_energy + potential_energy
+        return total_energy
+    
     def __init__(self, mass, sun, venus, earth, mars, timestep, v=1):
         self.earth = earth
         self.x = self.earth.x[0]
@@ -95,3 +112,9 @@ class Probe:
             self.arr_x.append(x)
             self.arr_y.append(y)
             self.arr_z.append(z)
+            
+            ### this is the energy conservation validation part, since the animation will loops until manually stop,
+            ### program will not automatically show the total mechanical energy.
+
+            # total_energy = self.total_mechanical_energy([sun, venus, mars], self.ptr)
+            # print(f"Total Mechanical Energy at step {self.ptr}: {total_energy}")
